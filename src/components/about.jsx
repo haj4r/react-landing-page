3b5ec91f36535aad8fa30ef6935d171b9c2d3edd
haj4r <2020855616@student.uitm.edu.my>
2024-08-12 13:55:20 +0800

    photos

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,7 +7,7 @@ export const About = (props) => {
         <div className="row">
           <div className="col-xs-12 col-md-5">
             {" "}
-            <img src="img/hajar.jpeg" className="img-responsive" alt="" />{" "}
+            <img src="img/hajar.jpg" className="img-responsive" alt="" />{" "}
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
